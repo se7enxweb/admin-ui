582cbedbed50cb447bdb1dae4735e7d83600c417
7x <info@se7enx.com>
2026-03-22 14:43:39 -0700

    Updated: Replacing key encore build process internal path resolution strings to point to forked vendor storage. Bugfix.

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addAliases({
-        '@ibexa-admin-ui': path.resolve('./vendor/ibexa/admin-ui'),
-        '@ibexa-admin-ui-helpers': path.resolve('./vendor/ibexa/admin-ui/src/bundle/Resources/public/js/scripts/helpers'),
-        '@ibexa-admin-ui-modules': path.resolve('./vendor/ibexa/admin-ui/src/bundle/ui-dev/src/modules'),
+        '@ibexa-admin-ui': path.resolve('./vendor/se7enxweb/admin-ui'),
+        '@ibexa-admin-ui-helpers': path.resolve('./vendor/se7enxweb/admin-ui/src/bundle/Resources/public/js/scripts/helpers'),
+        '@ibexa-admin-ui-modules': path.resolve('./vendor/se7enxweb/admin-ui/src/bundle/ui-dev/src/modules'),
     });
 };

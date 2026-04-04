f095d48f67cf1cd1a3ea7978ae38729e42fb8fd5
7x <info@se7enx.com>
2026-04-04 08:45:50 -0700

    Fix vendor paths: vendor/ibexa/admin-ui-assets -> vendor/se7enxweb/admin-ui-assets
    ibexa.css.config.js and ibexa.js.config.js had hardcoded absolute paths to
    vendor/ibexa/admin-ui-assets for flatpickr and leaflet vendor files.
    This package replaces ibexa/admin-ui-assets with se7enxweb/admin-ui-assets,
    so the paths must reference the fork location.

--- src/bundle/Resources/encore/ibexa.css.config.js
+++ src/bundle/Resources/encore/ibexa.css.config.js
@@ -5,16 +5,16 @@ module.exports = (Encore) => {
         path.resolve(__dirname, '../public/scss/ibexa-bootstrap.scss'),
         path.resolve(__dirname, '../public/scss/ibexa.scss'),
         path.resolve(__dirname, '../public/scss/ui/ibexa-modules.scss'),
-        path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/flatpickr/dist/flatpickr.min.css'),
+        path.resolve('./vendor/se7enxweb/admin-ui-assets/src/bundle/Resources/public/vendors/flatpickr/dist/flatpickr.min.css'),
         path.resolve(
-            './vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/flatpickr/dist/plugins/confirmDate/confirmDate.css',
+            './vendor/se7enxweb/admin-ui-assets/src/bundle/Resources/public/vendors/flatpickr/dist/plugins/confirmDate/confirmDate.css',
         ),
     ])
         .addEntry('ibexa-admin-ui-content-edit-parts-css', [
-            path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/leaflet/dist/leaflet.css'),
+            path.resolve('./vendor/se7enxweb/admin-ui-assets/src/bundle/Resources/public/vendors/leaflet/dist/leaflet.css'),
         ])
         .addEntry('ibexa-admin-ui-location-view-css', [
-            path.resolve('./vendor/ibexa/admin-ui-assets/src/bundle/Resources/public/vendors/leaflet/dist/leaflet.css'),
+            path.resolve('./vendor/se7enxweb/admin-ui-assets/src/bundle/Resources/public/vendors/leaflet/dist/leaflet.css'),
         ])
         .addEntry('ibexa-admin-ui-security-base-css', [
             path.resolve(__dirname, '../public/scss/ibexa-bootstrap.scss'),

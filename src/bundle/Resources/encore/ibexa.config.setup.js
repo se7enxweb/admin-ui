6fec541619e668286b476e82303579985871cd95
7x <info@se7enx.com>
2026-04-07 13:40:41 -0700

    fix: resolve @ibexa-admin-ui alias to se7enxweb/admin-ui instead of ibexa/admin-ui
    The encore setup file was pointing the @ibexa-admin-ui webpack alias at
    vendor/ibexa/admin-ui which does not exist in projects using the se7enxweb fork.
    All JS/icon/helper references using @ibexa-admin-ui failed to resolve, causing
    109 build errors in yarn ibexa:build.
    
    Fix: alias to vendor/se7enxweb/admin-ui (this package) so that all downstream
    ibexa:build runs can resolve JS module references correctly.

--- src/bundle/Resources/encore/ibexa.config.setup.js
+++ src/bundle/Resources/encore/ibexa.config.setup.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addAliases({
-        '@ibexa-admin-ui': path.resolve('./vendor/ibexa/admin-ui'),
-        '@ibexa-admin-ui-modules': path.resolve('./vendor/ibexa/admin-ui/src/bundle/ui-dev/src/modules'),
+        '@ibexa-admin-ui': path.resolve('./vendor/se7enxweb/admin-ui'),
+        '@ibexa-admin-ui-modules': path.resolve('./vendor/se7enxweb/admin-ui/src/bundle/ui-dev/src/modules'),
     });
 };
